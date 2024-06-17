3df4f50ad6751a5bfdb09e5cc43b1fcfe6f94c87
manavcodaty <manav.codaty@gmail.com>
2024-06-17 11:46:32 +0400

    refactor: Update widget_test.dart to disable debug mode banner
    The widget_test.dart file has been modified to disable the debug mode banner by setting the `debugShowCheckedModeBanner` property to `false` in the `MemoApp` widget. This change improves the visual appearance of the app during testing.
    
    Note: Please remove any meta information such as issue references, tags, or author names before committing.

--- memo/test/widget_test.dart
+++ memo/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:Memo/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MemoApp());
+    await tester.pumpWidget(MemoApp(debugShowCheckedModeBanner: false,));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
