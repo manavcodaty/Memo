ec32733ef43e32b18ea0ca324583a737f0484fa7
manavcodaty <manav.codaty@gmail.com>
2024-06-17 11:15:52 +0400

    Update dependencies and configurations for iOS and macOS builds

--- memo/test/widget_test.dart
+++ memo/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:memo/main.dart';
+import 'package:Memo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
